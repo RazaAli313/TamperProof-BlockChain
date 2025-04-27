00da6822f0ab7c24d4f02d821c8a3fcc6671ba5d
Raza Ali <136004622+RazaAli313@users.noreply.github.com>
2025-04-27 08:17:07 +0500

    Update main.py

--- backend/blockchain-service/app/main.py
+++ backend/blockchain-service/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, UploadFile
 import hashlib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000","http://localhost:8002"],  # You can replace "*" with specific frontend URL (e.g., ["http://localhost:3000"])
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 def calculate_hash(content: bytes) -> str:
     sha = hashlib.sha256()
